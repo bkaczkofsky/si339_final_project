2f01f528b559cb7636cf1f2c4c1e3274f34177b3
bkaczkofsky <78938341+bkaczkofsky@users.noreply.github.com>
2022-12-04 17:41:40 -0500

    Add files via upload

---
+++ js/button.js
@@ -0,0 +1,18 @@
+function myFunction() {
+
+    document.getElementById("demo1").innerHTML = " In Jazz there’s a balance of written material and soul. It’s like our Constitution can be amended, yet some stuff is not going to change. But you have a lot of stuff that you can change, and you can change things based on what is already written";
+    
+    document.getElementById("demo2").innerHTML = "Improvising is like a conversation. It is more unpredictable. You don’t know what’s going to happen. If you were talking to someone, I mean, you don’t just start randomly talking about anything off the top of your head, you have a topic that you’re dealing with. That’s what an improvised solo is, it’s a conversation with the rhythm section and everyone who is soloing at the time.";
+    
+    document.getElementById("demo3").innerHTML = "It is an honor to be able to lead the way for the very first time into the theater that is the heart of the dream home for America’s music.";
+    
+    document.getElementById("demo4").innerHTML = "There’s an element in love in all of Jazz. This type of music is our way of life. We have difficult times where we disagree on something or we have arguments about music all the time, but there’s an underlying love and respect. It’s always about the music.";
+    
+    document.getElementById("demo5").innerHTML = "Our music is not a dictatorial type of music. It’s very democratic.";
+    
+    document.getElementById("demo6").innerHTML = "The music comes to people in different ways. It comes to me in different ways. Sometimes it may come as a set of chord progressions that I build upon. Sometimes it might come as a single melody. Sometimes it may come as an entire piece together.";
+    
+    document.getElementById("demo7").innerHTML = "Brass bands and Italian folk songs, Irish fiddle tunes added to the mix. Then came Ragtime, the Blues, and the sacred music of the black church can I get a witness. Amen. By the end of the 19th century, all that was in the air, for New Orleans musicians to grab, to absorb, to transform, and the eventual result would be a brand new art. Not spirituals or Blues or Ragtime, but everything, everything all at once. Jazz.";
+    
+    document.getElementById("demo8").innerHTML = "Duke Ellington once said music is neither new or old. It’s either good or the other kind. We play all of Jazz, as much as we can, we say “all Jazz is modern”. And we may play newer songs or we may play Duke Ellington. We may play a song that I wrote last night. But we feel that all of it is important and worth hearing, and we’re going to try to swing and play with the same soul on all of the music.";
+  }
